0cd92df2d31d878ef672fb60554711ab94983940
Kami <kami.labs@proton.me>
2024-08-10 14:57:31 +0200

    v1

--- HTML/dist/assets/js/app.js
+++ HTML/dist/assets/js/app.js
@@ -1,22 +1,3 @@
-/* Template Name: Evea - Responsive App Landing Tailwind CSS Template
-   Author: Zoyothemes
-   Email: zoyothemes@gmail.com
-   Website: https://zoyothemes.com
-   Version: 1.0.0
-   Created: April 2024
-   File Description: Main JS file of the template
-*/
-
-/*********************************/
-/*         INDEX                 */
-/*================================
- *     01.  Sticky Navbar         *
- *     02.  Navbar active         *
- *     03.  Back to top           *
- *     04.  Accordions            *
- *     05.  Lucide Icons          *
- ================================*/
-
 /*********************/
 /*   Sticky Navbar   */
 /*********************/
@@ -39,7 +20,6 @@ window.addEventListener("scroll", (ev) => {
   windowScroll();
 });
 
-
 /*********************/
 /*   Navbar Active   */
 /*********************/
@@ -59,7 +39,6 @@ try {
     offset: 80,
   });
 } catch (error) {}
-
 
 /*********************/
 /*   Menu Collapse   */
@@ -94,25 +73,27 @@ window.toggleCollapse = toggleCollapse;
 /*********************/
 /*   Back To Top     */
 /*********************/
-var mybutton = document.getElementById("back-to-top");
-window.onscroll = function () {
-  scrollFunction();
-};
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopButton = document.getElementById("backToTop");
 
-function scrollFunction() {
-  if(mybutton!=null){
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-      mybutton.style.display = "inline-flex";
+  // Show or hide the button based on scroll position
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      // Show the button after scrolling down 300px
+      backToTopButton.style.display = "flex";
     } else {
-      mybutton.style.display = "none";
+      backToTopButton.style.display = "none";
     }
-  }
-}
+  });
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+  // Scroll smoothly to the top when the button is clicked
+  backToTopButton.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
 
 /*********************/
 /*   Accordions      */
@@ -120,140 +101,237 @@ function topFunction() {
 try {
   const Default = {
     alwaysOpen: false,
-    activeClasses: 'bg-white text-black rounded-xl',
-    inactiveClasses: 'text-dark',
-    onOpen: () => { },
-    onClose: () => { },
-    onToggle: () => { }
-  }
-  
+    activeClasses: "bg-white text-black rounded-xl",
+    inactiveClasses: "text-dark",
+    onOpen: () => {},
+    onClose: () => {},
+    onToggle: () => {},
+  };
+
   class Accordion {
     constructor(items = [], options = {}) {
-      this._items = items
-      this._options = { ...Default, ...options }
-      this._init()
+      this._items = items;
+      this._options = { ...Default, ...options };
+      this._init();
     }
 
     _init() {
       if (this._items.length) {
         // show accordion item based on click
-        this._items.map(item => {
-
+        this._items.map((item) => {
           if (item.active) {
-            this.open(item.id)
+            this.open(item.id);
           }
 
-          item.triggerEl.addEventListener('click', () => {
-            this.toggle(item.id)
-          })
-        })
+          item.triggerEl.addEventListener("click", () => {
+            this.toggle(item.id);
+          });
+        });
       }
     }
 
     getItem(id) {
-      return this._items.filter(item => item.id === id)[0]
+      return this._items.filter((item) => item.id === id)[0];
     }
 
     open(id) {
-      const item = this.getItem(id)
+      const item = this.getItem(id);
 
       // don't hide other accordions if always open
       if (!this._options.alwaysOpen) {
-        this._items.map(i => {
+        this._items.map((i) => {
           if (i !== item) {
-            i.triggerEl.classList.remove(...this._options.activeClasses.split(" "))
-            i.triggerEl.classList.add(...this._options.inactiveClasses.split(" "))
-            i.targetEl.classList.add('hidden')
-            i.triggerEl.setAttribute('aria-expanded', false)
-            i.active = false
+            i.triggerEl.classList.remove(
+              ...this._options.activeClasses.split(" ")
+            );
+            i.triggerEl.classList.add(
+              ...this._options.inactiveClasses.split(" ")
+            );
+            i.targetEl.classList.add("hidden");
+            i.triggerEl.setAttribute("aria-expanded", false);
+            i.active = false;
 
             // rotate icon if set
             if (i.iconEl) {
-              i.iconEl.classList.remove('rotate-180')
+              i.iconEl.classList.remove("rotate-180");
             }
           }
-        })
+        });
       }
 
       // show active item
-      item.triggerEl.classList.add(...this._options.activeClasses.split(" "))
-      item.triggerEl.classList.remove(...this._options.inactiveClasses.split(" "))
-      item.triggerEl.setAttribute('aria-expanded', true)
-      item.targetEl.classList.remove('hidden')
-      item.active = true
+      item.triggerEl.classList.add(...this._options.activeClasses.split(" "));
+      item.triggerEl.classList.remove(
+        ...this._options.inactiveClasses.split(" ")
+      );
+      item.triggerEl.setAttribute("aria-expanded", true);
+      item.targetEl.classList.remove("hidden");
+      item.active = true;
 
       // rotate icon if set
       if (item.iconEl) {
-        item.iconEl.classList.add('rotate-180')
+        item.iconEl.classList.add("rotate-180");
       }
 
       // callback function
-      this._options.onOpen(this, item)
+      this._options.onOpen(this, item);
     }
 
     toggle(id) {
-      const item = this.getItem(id)
+      const item = this.getItem(id);
 
       if (item.active) {
-        this.close(id)
+        this.close(id);
       } else {
-        this.open(id)
+        this.open(id);
       }
 
       // callback function
-      this._options.onToggle(this, item)
+      this._options.onToggle(this, item);
     }
 
     close(id) {
-      const item = this.getItem(id)
+      const item = this.getItem(id);
 
-      item.triggerEl.classList.remove(...this._options.activeClasses.split(" "))
-      item.triggerEl.classList.add(...this._options.inactiveClasses.split(" "))
-      item.targetEl.classList.add('hidden')
-      item.triggerEl.setAttribute('aria-expanded', false)
-      item.active = false
+      item.triggerEl.classList.remove(
+        ...this._options.activeClasses.split(" ")
+      );
+      item.triggerEl.classList.add(...this._options.inactiveClasses.split(" "));
+      item.targetEl.classList.add("hidden");
+      item.triggerEl.setAttribute("aria-expanded", false);
+      item.active = false;
 
       // rotate icon if set
       if (item.iconEl) {
-        item.iconEl.classList.remove('rotate-180')
+        item.iconEl.classList.remove("rotate-180");
       }
 
       // callback function
-      this._options.onClose(this, item)
+      this._options.onClose(this, item);
     }
   }
-  
-  window.Accordion = Accordion;
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('[data-accordion]').forEach(accordionEl => {
-      const alwaysOpen = accordionEl.getAttribute('data-accordion')
-      const activeClasses = accordionEl.getAttribute('data-active-classes')
-      const inactiveClasses = accordionEl.getAttribute('data-inactive-classes')
 
-      const items = []
-      accordionEl.querySelectorAll('[data-accordion-target]').forEach(el => {
+  window.Accordion = Accordion;
+
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("[data-accordion]").forEach((accordionEl) => {
+      const alwaysOpen = accordionEl.getAttribute("data-accordion");
+      const activeClasses = accordionEl.getAttribute("data-active-classes");
+      const inactiveClasses = accordionEl.getAttribute("data-inactive-classes");
+
+      const items = [];
+      accordionEl.querySelectorAll("[data-accordion-target]").forEach((el) => {
         const item = {
-          id: el.getAttribute('data-accordion-target'),
+          id: el.getAttribute("data-accordion-target"),
           triggerEl: el,
-          targetEl: document.querySelector(el.getAttribute('data-accordion-target')),
-          iconEl: el.querySelector('[data-accordion-icon]'),
-          active: el.getAttribute('aria-expanded') === 'true' ? true : false
-        }
-        items.push(item)
-      })
+          targetEl: document.querySelector(
+            el.getAttribute("data-accordion-target")
+          ),
+          iconEl: el.querySelector("[data-accordion-icon]"),
+          active: el.getAttribute("aria-expanded") === "true" ? true : false,
+        };
+        items.push(item);
+      });
 
       new Accordion(items, {
-        alwaysOpen: alwaysOpen === 'open' ? true : false,
+        alwaysOpen: alwaysOpen === "open" ? true : false,
         activeClasses: activeClasses ? activeClasses : Default.activeClasses,
-        inactiveClasses: inactiveClasses ? inactiveClasses : Default.inactiveClasses
-      })
-    })
-  })
-} catch (error) { }
-
+        inactiveClasses: inactiveClasses
+          ? inactiveClasses
+          : Default.inactiveClasses,
+      });
+    });
+  });
+} catch (error) {}
 
 /*********************/
 /*   Lucide Icons   */
 /*********************/
 lucide.createIcons();
+
+/*********************/
+/*   Contact Form   */
+/*********************/
+document.addEventListener("DOMContentLoaded", function () {
+  const notification = document.getElementById("notification");
+
+  function showNotification(message, isError = false) {
+    notification.textContent = message;
+    notification.className = `notification ${isError ? "error" : ""} show`;
+
+    // Hide the notification after 3 seconds
+    setTimeout(() => {
+      notification.className = "notification";
+    }, 3000);
+  }
+
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const fullName = document.getElementById("formFullName").value.trim();
+      const email = document.getElementById("formEmail").value.trim();
+      const phone = document.getElementById("formPhone").value.trim();
+      const reason = document.getElementById("formReason").value.trim();
+      const message = document.getElementById("formMessages").value.trim();
+
+      // Validate email with a stricter regex
+      const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      if (!emailPattern.test(email)) {
+        showNotification("Please enter a valid email address.", true);
+        return;
+      }
+
+      // Check minimum length for the message
+      if (message.length < 20) {
+        showNotification(
+          "Your message must be at least 20 characters long.",
+          true
+        );
+        return;
+      }
+
+      const webhookURL =
+        "https://discord.com/api/webhooks/1270801475584393297/tzSYkHpUexe_xSvvQYIswabKLlLNR2bRDU-aMDRR9lCNNtzqDh6JN_SosvEBoQA7mwKi";
+
+      const payload = {
+        content: `**New Contact Form Submission**\n**Full Name:** ${fullName}\n**Email:** ${email}\n**Phone:** ${phone}\n**Reason for Contact:** ${reason}\n**Message:** ${message}`,
+      };
+
+      // Ensure all fields are filled out
+      if (!fullName || !email || !reason || !message) {
+        showNotification("Please fill out all required fields.", true);
+        return;
+      }
+
+      fetch(webhookURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      })
+        .then((response) => {
+          if (response.ok) {
+            showNotification("Your message has been sent successfully!");
+            document.getElementById("contactForm").reset();
+          } else {
+            return response.json().then((data) => {
+              console.error("Error details:", data);
+              showNotification(
+                "There was a problem with your submission. Please try again.",
+                true
+              );
+            });
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          showNotification(
+            "There was a problem with your submission. Please try again.",
+            true
+          );
+        });
+    });
+});
